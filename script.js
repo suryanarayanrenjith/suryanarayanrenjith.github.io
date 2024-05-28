613b444433d831386b650ccdcdc491772e8a5077
Suryanarayan Renjith <141025553+suryanarayanrenjith@users.noreply.github.com>
2024-05-28 17:56:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const linksContainer = document.getElementById("links-container");
+const iconContainer = document.createElement("div");
+iconContainer.classList.add("icon-container");
+
+// Email link with icon
+const emailLinkItem = document.createElement("div");
+emailLinkItem.classList.add("link-item");
+const emailLinkAnchor = document.createElement("a");
+emailLinkAnchor.href = "mailto:suryanarayanrenjith@gamil.com";
+emailLinkAnchor.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-9 10L3 7v10h18V7l-9 6zm0-2.4L5.6 7H18.4L12 10.6z"/></svg> Email';
+emailLinkItem.appendChild(emailLinkAnchor);
+linksContainer.appendChild(emailLinkItem);
+
+
+// GitHub link with icon
+const githubLinkItem = document.createElement("div");
+githubLinkItem.classList.add("link-item");
+const githubLinkAnchor = document.createElement("a");
+githubLinkAnchor.href = "https://github.com/suryanarayanrenjith";
+githubLinkAnchor.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.88 8.17 6.84 9.49.5.09.68-.21.68-.48 0-.24-.01-.88-.01-1.75-2.78.5-3.37-1.34-3.37-1.34-.46-1.16-1.12-1.47-1.12-1.47-.91-.62.07-.61.07-.61 1 .07 1.53.71 1.53.71.89 1.53 2.34 1.09 2.91.83.09-.64.35-1.09.64-1.34-2.22-.25-4.56-1.12-4.56-5 0-1.1.39-2 1.03-2.71-.1-.25-.45-1.27.1-2.65 0 0 .84-.27 2.76 1.02a9.61 9.61 0 0 1 2.51-.34c.85 0 1.71.11 2.51.34 1.92-1.29 2.76-1.02 2.76-1.02.55 1.38.2 2.4.1 2.65.64.7 1.03 1.57 1.03 2.71 0 3.89-2.34 4.75-4.57 5 .36.31.68.91.68 1.84 0 1.33-.01 2.41-.01 2.73 0 .27.18.58.68.48C19.12 20.17 22 16.42 22 12c0-5.52-4.48-10-10-10z"/></svg> GitHub';
+githubLinkItem.appendChild(githubLinkAnchor);
+linksContainer.appendChild(githubLinkItem);
+
+
+// Twitter link with icon
+const twitterLinkItem = document.createElement("div");
+twitterLinkItem.classList.add("link-item");
+const twitterLinkAnchor = document.createElement("a");
+twitterLinkAnchor.href = "https://x.com/_suryanarayanr";
+twitterLinkAnchor.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><text x="0" y="24" font-family="Arial" font-size="24">&#x1D54F;</text></svg> X';
+twitterLinkItem.appendChild(twitterLinkAnchor);
+linksContainer.appendChild(twitterLinkItem);
+
+// LinkedIn link with icon
+const linkedinLinkItem = document.createElement("div");
+linkedinLinkItem.classList.add("link-item");
+const linkedinLinkAnchor = document.createElement("a");
+linkedinLinkAnchor.href = "https://linkedin.com/in/suryanarayan-renjith";
+linkedinLinkAnchor.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4.5 22h-4.5v-14.578h4.5v14.578zm-2.25-16.44c-1.617 0-2.919-1.311-2.919-2.922s1.302-2.922 2.919-2.922 2.919 1.311 2.919 2.922-1.302 2.922-2.919 2.922zm17.25 16.44h-4.5v-7.395c0-1.755-.033-4.018-2.438-4.018-2.443 0-2.82 1.905-2.82 3.875v7.538h-4.5v-14.578h4.363v2.053h.062c.609-1.158 2.104-2.383 4.301-2.383 4.602 0 5.462 3.033 5.462 6.979v8.928z"/></svg> LinkedIn';
+linkedinLinkItem.appendChild(linkedinLinkAnchor);
+linksContainer.appendChild(linkedinLinkItem);
+
+  
+  links.forEach(link => {
+    const linkItem = document.createElement("div");
+    linkItem.classList.add("link-item");
+    
+    const linkAnchor = document.createElement("a");
+    linkAnchor.textContent = link.name;
+    linkAnchor.href = link.url;
+    
+    linkItem.appendChild(linkAnchor);
+    linksContainer.appendChild(linkItem);
+  });
+
+  linksContainer.appendChild(iconContainer);
+  
+  
