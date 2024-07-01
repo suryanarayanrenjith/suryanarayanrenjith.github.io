4de366254b5e98142d26786f1ab656e741c54a6a
Suryanarayan Renjith <141025553+suryanarayanrenjith@users.noreply.github.com>
2024-07-01 10:19:33 +0530

    Update script.js

--- Links/script.js
+++ Links/script.js
@@ -1,3 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const footerText = document.getElementById('footer-text');
+    const currentYear = new Date().getFullYear();
+    
+    footerText.textContent = `© ${currentYear} Suryanarayan Renjith. All rights reserved.`;
+    
+    function changeTextWithDelay(newText) {
+        footerText.style.opacity = '0';
+        setTimeout(() => {
+            footerText.textContent = newText;
+            footerText.style.opacity = '1';
+        }, 300);
+    }
+    
+    footerText.addEventListener('mouseover', () => {
+        changeTextWithDelay('Music Credits: Suryanarayan Renjith');
+    });
+    
+    footerText.addEventListener('mouseout', () => {
+        changeTextWithDelay(`© ${currentYear} Suryanarayan Renjith. All rights reserved.`);
+    });
+    
+    footerText.style.transition = 'opacity 0.3s ease';
+    });
         const canvas = document.getElementById('3dCanvas');
         const ctx = canvas.getContext('2d');
         canvas.width = window.innerWidth;
