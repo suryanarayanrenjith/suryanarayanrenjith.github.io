357470d85c28379e3980c9fa4e694b19a9cef36a
Suryanarayan Renjith <141025553+suryanarayanrenjith@users.noreply.github.com>
2025-03-29 15:23:15 +0530

    Update script.js

--- Resume/script.js
+++ Resume/script.js
@@ -57,7 +57,7 @@ const scalerStd  = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 let model = null;
 async function loadModel() {
   if (!model) {
-    const modelPath = '/assets/tfjs_model/model.json';
+    const modelPath = 'https://model.suryanarayanrenjith.workers.dev/model.json';
     model = await tf.loadLayersModel(modelPath);
     console.log("Model loaded successfully.");
   }
