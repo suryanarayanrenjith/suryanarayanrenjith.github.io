79f0f0b4ebda55716695bc9a5e7dce3286de7aec
suryanarayanrenjith <suryanarayanrenjith@users.noreply.github.com>
2026-02-28 19:52:22 +0530

    feat: enhance preloader and control panel

--- blocker.js
+++ blocker.js
@@ -1,229 +1,264 @@
 (function() {
     'use strict';
 
-    function addCaptureListener(target, evt, fn) {
-        if (target.addEventListener) {
+    function cap(target, evt, fn) {
+        try {
             target.addEventListener(evt, fn, { capture: true, passive: false });
-        } else if (target.attachEvent) {
-            target.attachEvent('on' + evt, fn);
+        } catch (e) {
+            try { target.attachEvent('on' + evt, fn); } catch (_) {}
         }
     }
 
-    var blockContext = function(e) {
+    function nuke() {
+        try { document.documentElement.innerHTML = ''; } catch (_) {}
+        try { window.location.replace('about:blank'); } catch (_) {}
+    }
+
+    try {
+        if (window.self !== window.top) {
+            nuke();
+            return;
+        }
+    } catch (e) {
+        nuke();
+        return;
+    }
+
+    var blockEvent = function(e) {
         e.preventDefault();
         e.stopImmediatePropagation();
         return false;
     };
-    addCaptureListener(document, 'contextmenu', blockContext);
-    addCaptureListener(window,   'contextmenu', blockContext);
-
+    cap(document, 'contextmenu', blockEvent);
+    cap(window,   'contextmenu', blockEvent);
     try {
         document.oncontextmenu = function() { return false; };
         window.oncontextmenu   = function() { return false; };
-    } catch (ignore) {}
+    } catch (_) {}
 
-    var styleTag = document.createElement('style');
-    styleTag.type = 'text/css';
-    styleTag.appendChild(document.createTextNode([
+    var styleEl = document.createElement('style');
+    styleEl.setAttribute('data-b', '1');
+    styleEl.textContent = [
         '* {',
         '  -webkit-user-select: none !important;',
         '  -moz-user-select: none !important;',
         '  -ms-user-select: none !important;',
         '  user-select: none !important;',
         '  -webkit-touch-callout: none !important;',
-        '}'
-    ].join('\n')));
-    var head = document.head || document.getElementsByTagName('head')[0] || document.documentElement;
-    head.appendChild(styleTag);
+        '}',
+        'img { pointer-events: none !important; -webkit-user-drag: none !important; }'
+    ].join('\n');
 
-    addCaptureListener(document, 'dragstart', function(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
+    var injectStyle = function() {
+        var h = document.head || document.getElementsByTagName('head')[0] || document.documentElement;
+        if (!document.querySelector('style[data-b="1"]')) {
+            h.appendChild(styleEl.cloneNode(true));
+        }
+    };
+    injectStyle();
+
+    var styleObserver = new MutationObserver(function() {
+        if (!document.querySelector('style[data-b="1"]')) {
+            injectStyle();
+        }
     });
+    try {
+        styleObserver.observe(document.documentElement, { childList: true, subtree: true });
+    } catch (_) {}
 
-    addCaptureListener(document, 'selectstart', function(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-    });
+    cap(document, 'dragstart',   blockEvent);
+    cap(document, 'selectstart', blockEvent);
 
-    addCaptureListener(document, 'mousedown', function(e) {
+    cap(document, 'mousedown', function(e) {
         if (e.detail > 1) {
             e.preventDefault();
             e.stopImmediatePropagation();
         }
     });
 
-    addCaptureListener(document, 'touchstart', function(e) {
+    cap(document, 'touchstart', function(e) {
         if (e.touches && e.touches.length > 1) {
             e.preventDefault();
             e.stopImmediatePropagation();
         }
     });
 
-    addCaptureListener(document, 'keydown', function(e) {
+    var blockedCombos = {
+        123: { any: true },    
+        73:  { ctrlShift: true },    
+        74:  { ctrlShift: true },    
+        67:  { ctrlShift: true },   
+        77:  { ctrlShift: true },    
+        69:  { ctrlShift: true },    
+        85:  { ctrl: true },         
+        83:  { ctrl: true },         
+        80:  { ctrl: true },         
+        65:  { ctrlOnly: true }      
+    };
+
+    function handleKeyBlock(e) {
         var key = e.keyCode || e.which;
-        var ctrlOrCmd = e.ctrlKey || e.metaKey;
+        var ctrl = e.ctrlKey || e.metaKey;
         var shift = e.shiftKey;
-        var alt = e.altKey;
+        var combo = blockedCombos[key];
+        if (!combo) return;
 
-        if (key === 123) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
+        var shouldBlock = false;
+        if (combo.any)                                      shouldBlock = true;
+        if (combo.ctrlShift && ctrl && shift)               shouldBlock = true;
+        if (combo.ctrl && ctrl)                              shouldBlock = true;
+        if (combo.ctrlOnly && ctrl && !shift)               shouldBlock = true;
 
-        if (ctrlOrCmd && shift && (key === 73 || key === 74 || key === 67)) {
+        if (shouldBlock) {
             e.preventDefault();
             e.stopImmediatePropagation();
+            e.stopPropagation();
+            return false;
         }
+    }
 
-        if (ctrlOrCmd && !shift && key === 85) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-
-        if (ctrlOrCmd && (key === 83 || key === 80)) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-
-        if (ctrlOrCmd && !shift && key === 65) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-
-        if (ctrlOrCmd && shift && (key === 83 || key === 85)) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-
-        if (ctrlOrCmd && shift && key === 77) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-
-        if (alt && key === 123) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-    });
-
-    addCaptureListener(window, 'keydown', function(e) {
-        var key = e.keyCode || e.which;
-        var ctrlOrCmd = e.ctrlKey || e.metaKey;
-        var shift = e.shiftKey;
-        var alt = e.altKey;
-
-        if (key === 123) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-        if (ctrlOrCmd && shift && (key === 73 || key === 74 || key === 67 || key === 77)) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-        if (ctrlOrCmd && (key === 85 || key === 83 || key === 80 || key === 65)) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-        if (ctrlOrCmd && shift && (key === 83 || key === 85)) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-        if (alt && key === 123) {
-            e.preventDefault();
-            e.stopImmediatePropagation();
-        }
-    });
-
+    cap(document, 'keydown', handleKeyBlock);
+    cap(window,   'keydown', handleKeyBlock);
+    cap(document, 'keyup',   handleKeyBlock);
+    cap(window,   'keyup',   handleKeyBlock);
 
     (function() {
-        if (!window.console) {
-            window.console = {};
-        }
+        if (!window.console) window.console = {};
         var methods = [
             'log', 'warn', 'info', 'error', 'debug', 'dir',
             'dirxml', 'group', 'groupCollapsed', 'groupEnd',
             'trace', 'assert', 'profile', 'profileEnd',
-            'time', 'timeEnd', 'count', 'table'
+            'time', 'timeEnd', 'timeLog', 'count', 'countReset',
+            'table', 'clear', 'exception'
         ];
+        var noop = function() {};
         methods.forEach(function(m) {
             try {
                 Object.defineProperty(console, m, {
-                    value: function() { /* no-op */ },
-                    writable: false,
+                    get: function() { return noop; },
+                    set: function() {},
                     configurable: false
                 });
-            } catch (err) {
-                console[m] = function() {};
+            } catch (_) {
+                try { console[m] = noop; } catch (__) {}
             }
         });
-    })();
 
-    (function() {
-        var nativeToString = Function.prototype.toString;
-        Function.prototype.toString = function() {
-            var name = (this.name || '').toLowerCase();
-            var blockedNames = ['detectdevtools', 'infinitedebugger', 'anonymous'];
-            for (var i = 0; i < blockedNames.length; i++) {
-                if (name.indexOf(blockedNames[i]) !== -1) {
-                    return 'function () { [native code] }';
-                }
+        var origCreate = document.createElement.bind(document);
+        document.createElement = function(tag) {
+            var el = origCreate(tag);
+            if (tag.toLowerCase() === 'iframe') {
+                var origAppend = el.appendChild;
+                setTimeout(function() {
+                    try {
+                        if (el.contentWindow && el.contentWindow.console) {
+                            methods.forEach(function(m) {
+                                try { el.contentWindow.console[m] = noop; } catch (_) {}
+                            });
+                        }
+                    } catch (_) {}
+                }, 0);
             }
-            return nativeToString.call(this);
+            return el;
         };
-
-        try {
-            Object.freeze(Object.prototype);
-            Object.freeze(Function.prototype);
-        } catch (e) {
-        }
     })();
 
     (function() {
-        var devtools = { open: false, orientation: null };
-        var threshold = 160;
+        var native = Function.prototype.toString;
+        var cloaked = new Set();
+        window.__cloak = function(fn) { cloaked.add(fn); };
 
-        function detectDevTools() {
-            var widthDiff = window.outerWidth - window.innerWidth > threshold;
+        Function.prototype.toString = function() {
+            if (cloaked.has(this)) return 'function () { [native code] }';
+            var name = (this.name || '').toLowerCase();
+            if (/detect|debugger|devtools|blocker|protect/.test(name)) {
+                return 'function () { [native code] }';
+            }
+            return native.call(this);
+        };
+        window.__cloak(Function.prototype.toString);
+        window.__cloak(handleKeyBlock);
+        window.__cloak(blockEvent);
+
+        try { Object.freeze(Function.prototype); } catch (_) {}
+    })();
+
+    (function() {
+        var wasOpen = false;
+
+        function checkDimensions() {
+            var dpr = window.devicePixelRatio || 1;
+            var threshold = Math.max(100, 160 / dpr);
+            var widthDiff  = window.outerWidth  - window.innerWidth  > threshold;
             var heightDiff = window.outerHeight - window.innerHeight > threshold;
-            var orientation = widthDiff ? 'vertical' : 'horizontal';
 
-            if ((window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized)
-                || widthDiff || heightDiff) {
-                if (!devtools.open || devtools.orientation !== orientation) {
-                    devtools.open = true;
-                    devtools.orientation = orientation;
-                    document.documentElement.innerHTML = '';
-                    window.location.replace('about:blank');
-                }
+            if (widthDiff || heightDiff) {
+                if (!wasOpen) { wasOpen = true; nuke(); }
             } else {
-                devtools.open = false;
-                devtools.orientation = null;
+                wasOpen = false;
             }
         }
 
-        setInterval(detectDevTools, 400);
-
-        (function infiniteDebugger() {
-            var start = Date.now();
+        function debuggerTrap() {
+            var t0 = performance.now();
             debugger;
-            var end = Date.now();
-            if (end - start > 100) {
-                document.documentElement.innerHTML = '';
-                window.location.replace('about:blank');
+            if (performance.now() - t0 > 80) nuke();
+        }
+
+
+        function consoleTrap() {
+            var sentinel = { toString: function() { nuke(); return ''; } };
+            try {
+                Object.defineProperty(sentinel, 'id', {
+                    get: function() { nuke(); }
+                });
+            } catch (_) {}
+            try { console.log('%c', sentinel); } catch (_) {}
+            try { console.dir(sentinel); } catch (_) {}
+        }
+
+        function elementTrap() {
+            var el = document.createElement('div');
+            var accessed = false;
+            Object.defineProperty(el, 'id', {
+                get: function() {
+                    if (!accessed) { accessed = true; nuke(); }
+                    return '';
+                }
+            });
+            try { console.log(el); } catch (_) {}
+        }
+
+        function firebugCheck() {
+            if (window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized) {
+                nuke();
             }
-            setTimeout(infiniteDebugger, 1000);
-        })();
+        }
+
+        setInterval(checkDimensions, 500);
+        setInterval(debuggerTrap, 1500);
+        setInterval(function() { consoleTrap(); elementTrap(); }, 3000);
+        setInterval(firebugCheck, 5000);
+
+        checkDimensions();
+        debuggerTrap();
     })();
 
-    (function() {
-        var obfuscatedPayload =
-            'KGZ1bmN0aW9uKCkgewogICAgdmFyIGxhcmdlTWFsbGljcm9iO1xuICAgIC8vIERlc2lsZS9qb2tlLXVwIHlvdXIgY29kZSBodXJ0IGNvbXBsZXRlbHkKICAgIC8vKirjvJjlsKbkpKCh9KSgpOw==';
+    cap(document, 'copy', function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
         try {
-            var decoded = atob(obfuscatedPayload);
-            new Function(decoded)();
-        } catch (e) {
-        }
-    })();
+            e.clipboardData.setData('text/plain', '');
+        } catch (_) {}
+    });
+    cap(document, 'cut', blockEvent);
+
+    setInterval(function() {
+        injectStyle();
+        try {
+            document.oncontextmenu = function() { return false; };
+            window.oncontextmenu   = function() { return false; };
+        } catch (_) {}
+    }, 2000);
 
 })();
